c5f24f65dc58cc0e0ef671960da725d75c129193
DmytroKondrashov <kondrashovd101@gmail.com>
2025-06-07 10:25:48 +0300

    finish the footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="content is-flex is-flex-direction-row is-justify-content-space-between is-align-items-center">
     <div class="is-flex is-flex-direction-row is-align-items-center">
-      <img src="/aniconnect_logo.png" alt="AniConnect" class="logo" style="width: 100%; height: 50px;" />
+      <img src="/aniconnect_logo.png" alt="AniConnect" class="logo" style="width: 100%; height: 66px;" />
     </div>
     <div class="is-flex is-flex-direction-row is-align-items-center is-justify-content-center">
       <p class="mb-0">Presented by</p>
@@ -10,8 +10,12 @@
         <p style="margin-left: 5px;">Dmytro Kondrashov</p>
       </a>
     </div>
-    <div class="is-flex is-flex-direction-row is-align-items-center">
-      <p>2025 AniConnect</p>
+    <div class="is-flex is-flex-direction-column is-align-items-center">
+      <p>Powered by:</p>
+      <div>
+        <img src="/Svelte-kit-horizontal.svg.png" alt="SvelteKit Logo" class="logo" style="height: 20px;" />
+        <img src="/bulma-logo.png" alt="Bulma Logo" class="logo" style="height: 20px;" />
+      </div>
     </div>
   </div>
 </footer>
