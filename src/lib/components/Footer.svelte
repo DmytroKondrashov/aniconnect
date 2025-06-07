7c371a015c85417c51511a923269d25338629481
DmytroKondrashov <kondrashovd101@gmail.com>
2025-06-07 09:57:21 +0300

    add basic footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,17 @@
+<footer class="footer">
+  <div class="content is-flex is-flex-direction-row is-justify-content-space-between is-align-items-center">
+    <div class="is-flex is-flex-direction-row is-align-items-center">
+      <img src="/aniconnect_logo.png" alt="AniConnect" class="logo" style="width: 100%; height: 50px;" />
+    </div>
+    <div class="is-flex is-flex-direction-row is-align-items-center is-justify-content-center">
+      <p class="mb-0">Presented by</p>
+      <a href="https://github.com/dmytrokondrashov" target="_blank" class="is-flex is-flex-direction-row is-align-items-center">
+        <img src="/25231.png" alt="Github Icon" class="logo" style="height: 20px; margin-left: 5px;" />
+        <p style="margin-left: 5px;">Dmytro Kondrashov</p>
+      </a>
+    </div>
+    <div class="is-flex is-flex-direction-row is-align-items-center">
+      <p>2025 AniConnect</p>
+    </div>
+  </div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import type { Snippet } from 'svelte';
 	import '../app.css';
 	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 
 	interface Props extends Record<string, unknown> {
 		children?: Snippet;
@@ -14,3 +15,5 @@
 <div class="container mb-6">
 	{@render children?.()}
 </div>
+
+<Footer/>
