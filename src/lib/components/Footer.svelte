115b08963e1d7e43e1c783b3ae1e5760a51195a6
Dmytro Kondrashov <kondrashovd101@gmail.com>
2025-08-14 22:04:55 +0300

    remove old code

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,25 +1,3 @@
-<!-- <footer class="footer">
-  <div class="content is-flex is-flex-direction-row is-justify-content-space-around is-align-items-center">
-    <div class="is-flex is-flex-direction-row is-align-items-center">
-      <img src="/aniconnect_logo_clear.png" alt="AniConnect" class="logo" style="width: 100%; height: 66px;" />
-    </div>
-    <div class="is-flex is-flex-direction-row is-align-items-center is-justify-content-center">
-      <p class="mb-0">Presented by</p>
-      <a href="https://github.com/dmytrokondrashov" target="_blank" class="is-flex is-flex-direction-row is-align-items-center">
-        <img src="/25231.png" alt="Github Icon" class="logo" style="height: 20px; margin-left: 5px;" />
-        <p style="margin-left: 5px;">Dmytro Kondrashov</p>
-      </a>
-    </div>
-    <div class="is-flex is-flex-direction-column is-align-items-center">
-      <p>Powered by:</p>
-      <div>
-        <img src="/Svelte-kit-horizontal.svg.png" alt="SvelteKit Logo" class="logo" style="height: 20px;" />
-        <img src="/bulma-logo.png" alt="Bulma Logo" class="logo" style="height: 20px;" />
-      </div>
-    </div>
-  </div>
-</footer> -->
-
 <footer class="bg-gray-100">
   <div class="flex flex-row justify-between items-center p-4">
     <div class="flex flex-row items-center max-w-60">
