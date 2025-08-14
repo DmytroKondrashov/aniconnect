56f56659e86177afde5ecbffdbfa0b255604ded7
Dmytro Kondrashov <kondrashovd101@gmail.com>
2025-08-14 22:02:30 +0300

    rewrite basic footer to Tailwind

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="footer">
+<!-- <footer class="footer">
   <div class="content is-flex is-flex-direction-row is-justify-content-space-around is-align-items-center">
     <div class="is-flex is-flex-direction-row is-align-items-center">
       <img src="/aniconnect_logo_clear.png" alt="AniConnect" class="logo" style="width: 100%; height: 66px;" />
@@ -15,6 +15,30 @@
       <div>
         <img src="/Svelte-kit-horizontal.svg.png" alt="SvelteKit Logo" class="logo" style="height: 20px;" />
         <img src="/bulma-logo.png" alt="Bulma Logo" class="logo" style="height: 20px;" />
+      </div>
+    </div>
+  </div>
+</footer> -->
+
+<footer class="bg-gray-100">
+  <div class="flex flex-row justify-between items-center p-4">
+    <div class="flex flex-row items-center max-w-60">
+      <img src="/aniconnect_logo_clear.png" alt="AniConnect" />
+    </div>
+    
+    <div class="flex flex-row items-center justify-center">
+      <p class="mb-0">Presented by</p>
+      <a href="https://github.com/dmytrokondrashov" target="_blank" class="flex flex-row items-center ml-1 mr-1 max-w-6">
+        <img src="/25231.png" alt="Github Icon" class="ml-1" />
+      </a>
+      <a href="https://github.com/dmytrokondrashov" class="ml-2">Dmytro Kondrashov</a>
+    </div>
+    
+    <div class="flex flex-row items-center">
+      <p>Powered by:</p>
+      <div class="flex max-w-30">
+        <img src="/Svelte-kit-horizontal.svg.png" alt="SvelteKit Logo" />
+        <img src="/bulma-logo.png" alt="Bulma Logo" class="ml-2" />
       </div>
     </div>
   </div>
