b6d98f824e2eeef2c0495404b996fc57bafd26cd
Dmytro Kondrashov <kondrashovd101@gmail.com>
2025-08-16 10:24:21 +0300

    add bottow margin

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,4 +1,4 @@
-<nav class="flex items-stretch justify-between bg-white px-4 py-2 shadow" aria-label="main navigation">
+<nav class="flex items-stretch justify-between bg-white px-4 py-2 shadow mb-4" aria-label="main navigation">
   <div class="max-w-38">
     <a href="/">
       <img src="/aniconnect_logo_clear.png" alt="Aniconnect Logo" />
