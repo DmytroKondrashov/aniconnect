7b2c47e8bb383c7476ff9101bcd02ec721670e1d
Dmytro Kondrashov <kondrashovd101@gmail.com>
2025-10-30 17:46:55 +0200

    refactor Footer component for improved layout and responsiveness using Tailwind CSS

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,22 +1,22 @@
-<footer class="bg-gray-100">
-  <div class="flex flex-row justify-between p-4">
-    <div class="flex flex-row items-center max-w-60">
-      <img src="/aniconnect_logo_clear.png" alt="AniConnect" />
+<footer class="bg-gray-100 py-4">
+  <div class="flex flex-row justify-between p-4 max-w-7xl mx-auto">
+    <div class="flex flex-row items-center">
+      <img src="/aniconnect_logo_clear.png" alt="AniConnect" class="h-6 w-auto object-contain flex-shrink-0" />
     </div>
     
     <div class="flex flex-row items-center justify-center">
       <p class="mb-0">Presented by</p>
-      <a href="https://github.com/dmytrokondrashov" target="_blank" class="flex flex-row items-center ml-1 mr-1 max-w-6">
-        <img src="/25231.png" alt="Github Icon" class="ml-1" />
+      <a href="https://github.com/dmytrokondrashov" target="_blank" class="flex flex-row items-center ml-1 mr-1">
+        <img src="/25231.png" alt="Github Icon" class="ml-1 h-5 w-auto object-contain flex-shrink-0" />
       </a>
       <a href="https://github.com/dmytrokondrashov" class="ml-2">Dmytro Kondrashov</a>
     </div>
     
     <div class="flex flex-row items-center">
       <p>Powered by:</p>
-      <div class="flex max-w-30">
-        <img src="/Svelte-kit-horizontal.svg.png" alt="SvelteKit Logo" />
-        <img src="/bulma-logo.png" alt="Bulma Logo" class="ml-2" />
+      <div class="flex items-center">
+        <img src="/Svelte-kit-horizontal.svg.png" alt="SvelteKit Logo" class="h-6 w-auto object-contain flex-shrink-0" />
+        <img src="/bulma-logo.png" alt="Bulma Logo" class="ml-2 h-6 w-auto object-contain flex-shrink-0" />
       </div>
     </div>
   </div>
